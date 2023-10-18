c8c2198b9471ef77f8fa94b5c43b682415af8945
Aseru Kevin Ziyada <109363782+lucy-kevin@users.noreply.github.com>
2023-10-18 07:22:54 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,24 @@
 y = 10
 z = 3
 print(y+z)
+powers= 5**3
+print(powers)
+
+cal = 10+5 ** 2// 5
+print(cal)
+
+basenum = 0b0101
+print(basenum)#decimal
+hexaNum = 0xff
+print(hexaNum)
+floatNUM = 1.72E5#1.73 into 10 to the power 5
+print(floatNUM)
+floatN = 1.73e-2
+print(floatN)
+
+name = 'Sam'
+
+if name == 'Sam':
+    print('Hello Sam')
+else:
+    print('sup men')
