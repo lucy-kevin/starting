ad760851ddf98fd578b0f695f5c322e2dae00c6e
Aseru Kevin Ziyada <109363782+lucy-kevin@users.noreply.github.com>
2023-10-20 08:03:20 +0300

    Update start.py

--- start.py
+++ start.py
@@ -22,3 +22,51 @@ if name == 'Sam':
     print('Hello Sam')
 else:
     print('sup men')
+
+_name ='Kevin'
+print(_name)
+
+#name&s = "kevin"// this brings an error
+#print(name&s)
+statement = " the lang 'python' is named after monty python"
+print(statement)
+#statement1 = " the lang "python" is named after monty python"
+#this is wrong
+statement1 = " the lang \"python\" is named' after monty python"
+print(statement1)
+into = """ Hello,
+i have an annoying lecture
+and in the moring"""
+print(into)
+print(_name[2])
+print(_name[2:5])
+print(_name[-2])
+print(_name[:4])
+print(_name[2:])
+x = "I"
+y = " am"
+z= " Kevin"
+print(x + y + z)
+z= z*4
+print(z)
+helo = "Hello" <= "hello"
+print(helo)
+helo = "Hello" == "hello"
+print(helo)
+
+#membership operators
+
+ask = "vi" in "Kevin"
+print(ask)
+ask1 = "iv" in "Kevin"
+print(ask1)
+
+ask2 = "vi" not in "Kevin"
+print(ask2)
+ask12 = "iv" not in "Kevin"
+print(ask12)
+
+age = 18
+print('my age is %d' % age)
+
+
